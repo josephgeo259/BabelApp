d0de344aa5384ba00027eb7a250e3325c03560b3
GeoJoseph <josephgeo259@gmail.com>
2018-05-29 17:04:58 -0400

    routes are showing correctly

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+
+    resources :users do
+
+      resources :blogs, :comments
+
+    end
+
+  end
+
 end
