9f4b936e0a36ca501b4579a154c7090d709d8a9f
GeoJoseph <josephgeo259@gmail.com>
2018-05-29 16:26:41 -0400

    seeds are made

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Blog.destroy_all
+Comment.destroy_all
+
+geo = User.create!(
+    name: "Geo", 
+    location: "Atlanta" 
+    spoken_languages: "Malayalam" 
+    learning_interest:"French")
+
+bestblog = Blog.create!(
+    title:"French",
+    post:"French is second only to English for the number of countries where it has official status – 32 as opposed to 45. And, with 56 members, La Francophonie is now larger than the Commonwealth, which has 53.French is also the only language, with English, that is taught in every country of the world, with 100 million students and 2 million teachers – 20 % of whom are outside of francophone countries.",
+    user_id: geo.id)
+
+comment = Comment.create!(
+    title: "France", 
+    description:"In 700-500BC the Celtic Gauls arrive in France. In 58-50BC Roman Emperor Julius Caesar defeats the Gauls and France becomes part of the Roman Empire until 476AD. French was ruled by kings for many centuries until the storming of the Bastille during the French Revolution in 1789. Then Napoleon becomes Emperor of the French Republic until he is sent to exile.", 
+    user_id: geo.id)
+
+joshi = User.create!(
+    name: "Joshi", 
+    location: "Japan" 
+    spoken_languages: "Japanese" 
+    learning_interest:"Spanish")
+
+newblog = Blog.create!(
+    title:"Spanish", 
+    post:"Spanish is a dominant language in the West. Spanish is spoken by 500 million people (the most spoken language in the world after Mandarin Chinese) and it’s growing more rapidly than many other languages. By 2050 Spanish will reach 600 million speakers. ", 
+    user_id: geo.id)
+
+comment = Comment.create!(
+    title: "Spain", 
+    description:"Spain was once a number of separate kingdoms with different languages – which were unified in the 15th century after the marriage of two Catholic monarchs Ferdinand of Aragon and Isabella of Castile. These kingdoms became the basis for many of the different regions in modern Spain. Today, there are 17 autonomous regions (15 on the mainland and the Balearic and Canary Islands), and two autonomous enclaves of Ceuta and Melilla in North Africa.", 
+    user_id: geo.id)
+
+annie = User.create!(
+    name: "Annie", 
+    location: "India" 
+    spoken_languages: "Hindi" 
+    learning_interest:"Swahili")
+
+myblog = Blog.create!(
+    title:"Swahilli", 
+    post:"a Bantu language widely used as a lingua franca in East Africa and having official status in several countries. The people of Zanzibar and nearby coastal regions, descendants of the original speakers of Swahili", 
+    user_id: geo.id)
+
+comment = Comment.create!(
+    title: "Zanzibar", 
+    description:"The name Zanzibar is an Arabic word that is translated as 'the coast of black people'. The Zanzibar archipelago is a series of islands on the Indian Ocean about 16-31 miles from the mainland Republic of Tanzania. It is a semi-autonomous region of the Tanzania. The island has the best beaches in the world - and this is not an exaggeration. If you love lazing around in the sun, then this is the place to go. Below are a few facts that will give you an overview of the Island country.", 
+    user_id: geo.id)
+
+    puts "DB:Seeded"
