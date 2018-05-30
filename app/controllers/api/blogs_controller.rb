8fdd6bf33d43bdc1a51d8e302ebf0eaece7b0174
GeoJoseph <josephgeo259@gmail.com>
2018-05-30 10:14:20 -0400

    blogs and comments controller

--- app/controllers/api/blogs_controller.rb
+++ app/controllers/api/blogs_controller.rb
@@ -1,2 +1,40 @@
 class Api::BlogsController < ApplicationController
+    
+def index
+    @blog = User.find(params[:user_id]).blog
+    render json: @blog
 end
+
+def show
+    @blog = Blog.find(params[:id])
+    render json: @blog
+end
+
+def destroy
+    @blog= Blog.find(params[:id])
+    @blog.destroy
+    render status: 200
+end
+
+def update
+    @user = User.find(params[:user_id])
+    @blog = Blog.find params[:id]
+    @blog.update!(blog_params)
+    render json: @blog
+end
+
+def create
+    @user = User.find(params[:user_id])
+    @blog = Blog.create!(blog_params)
+    render json: @blog
+end
+
+  
+
+private
+ def procedure_params
+    params.require(:blogs).permit(:)
+end
+end
+end
+
--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,40 @@
 class Api::CommentsController < ApplicationController
+    
+
+def index
+    @comment = User.find(params[:user_id]).comment
+    render json: @comment
+end
+
+def show
+    @comment = Comment.find(params[:id])
+    render json: @comment
+end
+
+def destroy
+    @comment= Comment.find(params[:id])
+    @comment.destroy
+    render status: 200
+end
+
+def update
+    @user = User.find(params[:user_id])
+    @comment = Comment.find params[:id]
+    @comment.update!(comment_params)
+    render json: @comment
+end
+
+def create
+    @user = User.find(params[:user_id])
+    @comment = Comment.create!(comment_params)
+    render json: @comment
+end
+
+  
+
+private
+ def procedure_params
+    params.require(:comments).permit(:)
+end
+end
 end
